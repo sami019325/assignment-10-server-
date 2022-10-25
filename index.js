d4fcf73f494d5097e2512e1f03b8b5593abee080
Kawsar Ahmed <kawsarmiahstdnt@gmail.com>
2022-10-25 16:17:22 +0600

    catagory added

--- index.js
+++ index.js
@@ -4,14 +4,14 @@
 // app.use(cors())
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
-/* const categories = require('./Data/category')
+const categories = require('./Data/category')
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
- */
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
