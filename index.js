72719303f267663da109a9bd5436668ed05bb0d0
Kawsar Ahmed <kawsarmiahstdnt@gmail.com>
2022-10-25 16:26:14 +0600

    course function added

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 
-// const cors = require('cors')
 
-// app.use(cors())
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
 
 const categories = require('./Data/category')
+const course = require('./Data/Course')
 
+app.use(cors())
 app.get('/', (req, res) => {
     res.send('Hello World!')
-})
+});
 
+app.get('/category', (req, res) => {
+    res.send(categories)
+});
+app.get('/course', (req, res) => {
+    res.send(course)
+});
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
