84b7d0e0d92d84f7b1d296380875877d5ade2097
Kawsar Ahmed <kawsarmiahstdnt@gmail.com>
2022-10-26 12:19:40 +0600

    course data added

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ app.get('/course', (req, res) => {
     res.send(course)
     console.log(course)
 });
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = course.find(n => n._id === id);
+    res.send(selectedCourse);
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
